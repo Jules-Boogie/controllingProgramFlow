4ffa3207ce6f3bf4874fa350110bb241c94f1b20
Juliet George <georgejuliet11@gmail.com>
2020-08-03 15:42:23 -0700

    implementing the same functions with a while loop

--- FixedPoints/func.py
+++ FixedPoints/func.py
@@ -6,7 +6,7 @@ Date: 8/3/2020
 """
 
 
-def fixed_points(tup):
+def fixed_points(tup): #for-loop
     """
     Returns a copy of tup, including only the "fixed points".
 
@@ -27,3 +27,30 @@ def fixed_points(tup):
         if pos == tup[pos]:
             fixed_point = fixed_point + (tup[pos],)
     return fixed_point
+
+
+def fixed_points(tup): #while-loop
+    """
+    Returns a copy of tup, including only the "fixed points".
+
+    A fixed point of a tuple is an element that is equal to its position in the tuple.
+    For example 0 and 2 are fixed points of (0,3,2).  The fixed points are returned in
+    the order that they appear in the tuple.
+
+    Examples:
+        fixed_points((0,3,2)) returns (0,2)
+        fixed_points((0,1,2)) returns (0,1,2)
+        fixed_points((2,1,0)) returns ()
+
+    Parameter tup: the tuple to copy
+    Precondition: tup is a tuple of ints
+    """
+    # You must use a while-loop, not a for-loop
+    res = ()
+    l = len(tup)
+    start = 0
+    while start < l:
+        if start == tup[start]:
+            res = res + (tup[start],)
+        start = start + 1
+    return res
--- Skip/func.py
+++ Skip/func.py
@@ -6,12 +6,12 @@ Date: 8/3/2020
 """
 
 
-def skip(s,n):
+def skip(s,n): #for-loop
     """
     Returns a copy of s, only including positions that are multiples of n
 
     A position is a multiple of n if pos % n == 0.
-    
+
     Examples:
         skip('hello world',1) returns 'hello world'
         skip('hello world',2) returns 'hlowrd'
@@ -30,3 +30,32 @@ def skip(s,n):
         if pos%n == 0:
             s1 = s1 + str(s[pos])
     return s1
+
+
+def skip(s,n): #while-loop
+    """
+    Returns a copy of s, only including positions that are multiples of n
+
+    A position is a multiple of n if pos % n == 0.
+
+    Examples:
+        skip('hello world',1) returns 'hello world'
+        skip('hello world',2) returns 'hlowrd'
+        skip('hello world',3) returns 'hlwl'
+        skip('hello world',4) returns 'hor'
+
+    Parameter s: the string to copy
+    Precondition: s is a nonempty string
+
+    Parameter n: the letter positions to accept
+    Precondition: n is an int > 0
+    """
+    # You must use a while-loop, not a for-loop
+    l = len(s)
+    start = 0
+    res = ""
+    while start < l:
+        if start % n == 0:
+            res = res + s[start]
+        start = start + 1
+    return res
